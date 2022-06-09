fe19f258378ec5479368dbe7ce7a989cd74776c4
herbowicz <herbowicz@gmail.com>
2022-06-09 10:22:11 +0200

    frontend blog

---
+++ src/queries/article/article.js
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+
+const ARTICLE_QUERY = gql`
+  query Article($slug: String!) {
+    articles(filters: { slug: { eq: $slug } }) {
+      data {
+        attributes {
+          slug
+          title
+          content
+          category {
+            data {
+              attributes {
+                slug
+                name
+              }
+            }
+          }
+          image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default ARTICLE_QUERY;
